10f57e9070348c2ed742f1bd9ff5a465331a696c
Ryan Harg <ryan.harg@mailbox.org>
2021-08-20 11:18:03 +0200

    Update dependencies
    - Exoplayer extensions
    - openid appauth

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -3,19 +3,17 @@ object Versions {
   const val jacoco = "0.8.7"
   const val unmock = "0.7.8"
   const val androidGradlePlugin = "7.0.0"
-  const val gradleAndroidJUnitJacocoPlugin = "0.16.0"
   const val gradleDependencyPlugin = "0.38.0"
 
   const val exoPlayer = "2.11.8"
-  const val exoPlayerExtensions = "2.11.4"
+  const val exoPlayerExtensions = "2.14.0"
   const val fuel = "2.3.1"
   const val gson = "2.8.7"
   const val powerPreference = "2.0.0"
-  const val openIdAppAuth = "0.9.1"
+  const val openIdAppAuth = "0.10.0"
 
   const val mockk = "1.12.0"
   const val strikt = "0.31.0"
-  const val androidXTest = "1.4.0"
   const val robolectric = "4.6.1"
   const val koin = "3.1.2"
 }
